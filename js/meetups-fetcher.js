8783d9aec7e0370dff6167a542caa8381019a7ab
Olirale <bardanca@gmail.com>
2025-04-19 21:40:14 +0200

    reorganizing where js script is

--- js/meetups-fetcher.js
+++ js/meetups-fetcher.js
@@ -1,42 +1,16 @@
-$().ready(function() {
-    // Templates
-    var pastMeetupTemplate = $.templates('#past-meetup-template');
-    var upcomingMeetupTemplate = $.templates('#upcoming-meetup-template');
 
-    // Utility functions
-    function formatDate(dateStr) {
-        // Original format YYYY-MM-DD
-        var parts = dateStr.split('-');
-        return parts[2] + '/' + parts[1] + '/' + parts[0];
-    }
 
-    // Upcoming events
-    $.ajax({
-        url: '//api.meetup.com/CorunaJUG/events?status=upcoming&fields=featured_photo',
-        dataType: 'jsonp',
-        jsonp: 'callback',
-        success: function(response) {
-            if (response.data.length > 0) {
-                $.each(response.data, function() {
-                    this.formated_date = formatDate(this.local_date) + ' ' + this.local_time;
-                    $('#upcoming-meetups-container').append(upcomingMeetupTemplate.render(this));
-                });   
-            } else {
-                $('#no-upcoming-meetup').show();
-            }
-        }
-    });
-
-    // Past events
-    $.ajax({
-        url: '//api.meetup.com/CorunaJUG/events?status=past&fields=featured_photo&page=6&desc=true',
-        dataType: 'jsonp',
-        jsonp: 'callback',
-        success: function(response) {
-            $.each(response.data, function() {
-                this.formated_date = formatDate(this.local_date) + ' ' + this.local_time;
-                $('#past-meetups-container').append(pastMeetupTemplate.render(this));
-            });
-        }
-    });
-});
+function getUpcomingMeetup(){
+    fetch(`/data/next.json?nocache=${Math.random()}`)
+        .then(response => {
+            if (!response.ok) throw new Error("Network response was not ok");
+            return response.json();
+        })
+        .then(meetupData => {
+            const template = $.templates("#meetupTemplate");
+            document.getElementById("upcoming-meetup").innerHTML = template.render(meetupData);
+        })
+        .catch(error => {
+            console.error("Failed to load JSON:", error);
+        });
+}
