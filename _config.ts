a0338db4641a694dfc78936ed6dce498ffd25690
Olirale <bardanca@gmail.com>
2025-11-19 00:38:07 +0100

    cambio en el workflow

--- _config.ts
+++ _config.ts
@@ -5,14 +5,13 @@ import lightningCSS from "lume/plugins/lightningcss.ts";
 const site = lume({
   src: "./src",
   dest: "./docs",
-  location: new URL("https://www.corunajug.org/"),
+  location: new URL("https://corunajug.org/"),
   jsFile: "/js/script.js",
   cssFile: "/css/styles.css"
 });
 
 site.add("css");
 site.add("img");
-site.add("CNAME", "CNAME");
 site.use(lightningCSS());
 
 export default site;
